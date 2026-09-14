af30514d9a7d604625cc82fede0f9086439140a9
Chase <st10433939@vcconnect.edu.za>
2026-09-14 13:03:29 +0200

    added meter reading model

---
+++ PROG7312_POE_P1/Models/MeterReading.cs
@@ -0,0 +1,41 @@
+﻿namespace PROG7312_POE_P1.Models
+{
+    public readonly struct MeterReading
+    {
+        public double Watts { get; }
+
+        public MeterReading(double watts) 
+        { 
+            Watts = watts; 
+        }
+
+        //Operator overloading
+        public static MeterReading operator +(
+            MeterReading first,
+            MeterReading second)
+        {
+            return new MeterReading ( first.Watts +  second.Watts );
+        }
+
+        public static MeterReading operator -(
+            MeterReading first,
+            MeterReading second)
+        {
+            return new MeterReading( first.Watts - second.Watts );
+        }
+
+        public static bool operator >(
+            MeterReading first,
+            MeterReading second)
+        {
+            return first.Watts > second.Watts;
+        }
+
+        public static bool operator <(
+            MeterReading first,
+            MeterReading second)
+        {
+            return first.Watts < second.Watts;
+        }
+    }
+}
