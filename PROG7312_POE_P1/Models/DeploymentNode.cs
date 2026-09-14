afec6a51a94f293dcb1deaadc83e12831559b84e
Chase <st10433939@vcconnect.edu.za>
2026-09-14 12:56:42 +0200

    added deployment node model

---
+++ PROG7312_POE_P1/Models/DeploymentNode.cs
@@ -0,0 +1,20 @@
+﻿namespace PROG7312_POE_P1.Models
+{
+    public class DeploymentNode
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Type { get; set; } = string.Empty;
+
+        public bool IsConfigured { get; set; }
+
+        public List<DeploymentNode> Children { get; set; } = new List<DeploymentNode>();
+    }
+
+    public class DeploymentValidationResult
+    {
+        public bool IsValid { get; set; }
+
+        public List<string> Errors { get; set; } = new List<string>();
+    }
+}
