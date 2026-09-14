c3d93dcdc1f2822d65b58d3fa0858bd2ffd99ec4
Chase <st10433939@vcconnect.edu.za>
2026-09-14 12:53:14 +0200

    added telemetry alert and packets models

---
+++ PROG7312_POE_P1/Models/TelemetryAlert.cs
@@ -0,0 +1,15 @@
+﻿namespace PROG7312_POE_P1.Models
+{
+    public class TelemetryAlert
+    {
+        public int SensorId { get; set; }
+
+        public string DeviceId { get; set; } = string.Empty;
+
+        public string Severity { get; set; } = "Info";
+
+        public string Message { get; set; } = string.Empty;
+
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ PROG7312_POE_P1/Models/TelemetryPackets.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PROG7312_POE_P1.Models
+{
+    public class TelemetryPacket<T> where T : struct
+    {
+        [Required]
+        public string DeviceId { get; set; } = string.Empty;
+
+        public T Value { get; set; }
+
+        public string Unit { get; set; } = string.Empty;
+
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    }
+}
