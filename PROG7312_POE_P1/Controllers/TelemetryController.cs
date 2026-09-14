c3eab4a896745efbd049aa9a0dc82355e79e50e4
Chase <st10433939@vcconnect.edu.za>
2026-09-14 12:52:19 +0200

    added telemetry controller and view with index

---
+++ PROG7312_POE_P1/Controllers/TelemetryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PROG7312_POE_P1.Controllers
+{
+    public class TelemetryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
