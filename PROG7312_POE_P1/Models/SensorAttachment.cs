34a4520f63b414d835fc8eadfbb71fc3432c5d36
Chase <st10433939@vcconnect.edu.za>
2026-09-14 12:59:22 +0200

    added sensor attachment model

---
+++ PROG7312_POE_P1/Models/SensorAttachment.cs
@@ -0,0 +1,17 @@
+﻿namespace PROG7312_POE_P1.Models
+{
+    public class SensorAttachment
+    {
+        public int Id { get; set; }
+
+        public string OriginalFileName { get; set; } = string.Empty;
+
+        public string StoredFileName { get; set; } = string.Empty;
+
+        public string RelativeUrl { get; set; } = string.Empty;
+
+        public long SizeBytes { get; set; }
+
+        public DateTime UploadedAt { get; set; } = DateTime.UtcNow;
+    }
+}
