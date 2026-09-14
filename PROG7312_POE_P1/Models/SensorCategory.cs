9110b24ad067d237b5c41676ea29e5232c57a942
Chase <st10433939@vcconnect.edu.za>
2026-09-14 20:11:48 +0200

    added more hydroponic specific categories

--- PROG7312_POE_P1/Models/SensorCategory.cs
+++ PROG7312_POE_P1/Models/SensorCategory.cs
@@ -2,8 +2,17 @@
 {
     public enum SensorCategory
     {
-        Environmental,
-        WaterSolution,
-        Actuator
+        WaterQuality,     // pH, EC, Dissolved Oxygen, Water Temp
+        Nutrients,        // Dosing volume, Tank Levels
+        Environmental,    // Ambient Temp, Humidity, VPD, CO2
+        Actuator          // Dosing pumps, Aeration, Solenoids, Grow lights
+    }
+
+    public enum SubsystemZone
+    {
+        Reservoir,
+        GrowingChannel,
+        DosingStation,
+        AmbientRoom
     }
 }
