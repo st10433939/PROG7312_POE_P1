c0c6ff0e1a25a5eea1b5aac8ca396d4e4b2596c3
Chase <st10433939@vcconnect.edu.za>
2026-09-14 13:01:55 +0200

    added sensor registration request model and sensor telemetry snapshot

---
+++ PROG7312_POE_P1/Models/SensorRegistrationRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PROG7312_POE_P1.Models
+{
+    public class SensorRegistrationRequest
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string DeviceId { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string Location { get; set; } = string.Empty;
+
+        [Required]
+        public SensorCategory Category { get; set; }
+    }
+}
---
+++ PROG7312_POE_P1/Models/SensorTelemetrySnapshot.cs
@@ -0,0 +1,11 @@
+﻿namespace PROG7312_POE_P1.Models
+{
+    public class SensorTelemetrySnapshot
+    {
+        public List<TelemetryPacket<float>> FloatReadings { get; set; } = new List<TelemetryPacket<float>>();
+
+        public List<TelemetryPacket<int>> IntegerReadings { get; set; } = new List<TelemetryPacket<int>>();
+
+        public List<TelemetryPacket<bool>> BooleanReadings { get; set; } = new List<TelemetryPacket<bool>>();
+    }
+}
