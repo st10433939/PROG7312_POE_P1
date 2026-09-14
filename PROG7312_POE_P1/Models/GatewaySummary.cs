43f221844a8ec5202474cbfbff1894dc77f599b2
Chase <st10433939@vcconnect.edu.za>
2026-09-14 13:02:50 +0200

    added gateway summary model

---
+++ PROG7312_POE_P1/Models/GatewaySummary.cs
@@ -0,0 +1,18 @@
+﻿namespace PROG7312_POE_P1.Models
+{
+    public class GatewaySummary
+    {
+        public int SensorCount { get; set; }
+
+        public int FloatReadingCount { get; set; }
+
+        public int IntegerReadingCount { get; set; }
+
+        public int BooleanReadingCount { get; set; }
+
+        public double CombinedPowerWatts { get; set; }
+
+        public bool HighPowerLoad { get; set; }
+
+    }
+}
